1e10fd4792ea18b4e7040541ef328c3e56421916
Nguyen Huan <ndthuan2266@gmail.com>
2019-06-16 12:17:49 +0700

    fix bug for release

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-export const DATABASE = process.env.DB || 'mongodb://localhost/nhienshop';
+export const DATABASE = process.env.DB || 'mongodb+srv://huanndt:thaihuan2266@cluster0-80u3z.mongodb.net/test?retryWrites=true&w=majority';
 export const secret = "RbBQqA6uF#msRF8s7h*?@=95HUm&DgMDd6zLFn4XzWQ6dtwXSJwBX#?gL2JWf!";
 export const default_pwd = "nhienclothingdn";
 export const default_ad_email = 'admin@nhienclothing.com';
